1bfcbb9e5164fb84b3737b36c17a01faa7dbb6dc
ElinaTodorova <elina.todorova93@gmail.com>
2023-10-25 15:23:07 +0200

    Working with api

--- frontend/src/components/carrouselMain/CarrouselContainer.jsx
+++ frontend/src/components/carrouselMain/CarrouselContainer.jsx
@@ -3,23 +3,39 @@ import { useAuth } from "../../contexts/AuthContext";
 
 export default function CarrouselContainer() {
   const token = useAuth();
-  const [newReleases, setNewReleases] = useState([]);
+  const [newReleases, setNewReleases] = useState(null);
 
-  useEffect(() => {
-    if (token !== null) {
+  const url = "https://api.spotify.com/v1/browse/new-releases";
+
+  const handleFetchData = async () => {
+    try {
       const params = {
         headers: {
           Authorization: `Bearer ${token}`,
         },
       };
+      const response = await fetch(url, params);
+      const data = await response.json();
+      setNewReleases(data.albums.items);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-      const url = "https://api.spotify.com/v1/browse/new-releases";
-
-      fetch(url, params)
-        .then((response) => response.json())
-        .then((data) => setNewReleases(data));
+  useEffect(() => {
+    if (token !== null) {
+      handleFetchData();
+      // fetch(url, params)
+      //   .then((response) => response.json())
+      //   .then((data) => setNewReleases(data.albums.items));
     }
   }, [token]);
 
-  return <div>{JSON.stringify(newReleases)}</div>;
+  return (
+    <div>
+      {newReleases.map((element) => {
+        return <div key={element.name}>{element.name}</div>;
+      })}
+    </div>
+  );
 }
