5201ca51b2b5875fb7a5108facf50db5e7687338
Nas-lmr <nasreddine-lamamra_student2023@wilder.school>
2023-11-07 16:56:07 +0100

    upadate US-5

---
+++ frontend/src/components/buttonRandom/ButtonRandom.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./ButtonRandom.module.css";
+
+export default function ButtonRandom() {
+  return (
+    <div className={styles.buttonPosition}>
+      <button type="button" id={styles["button-random"]}>
+        Surprise me !!
+      </button>
+    </div>
+  );
+}
