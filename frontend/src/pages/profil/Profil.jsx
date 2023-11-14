e6095c38395ca2b0d96697d5bab7aa1737f4a5c7
AXEL LARESE <axelarese@gmail.com>
2023-11-14 16:33:17 +0100

    Réalisation du formulaire de connexion et mise en page CSS

--- frontend/src/pages/profil/Profil.jsx
+++ frontend/src/pages/profil/Profil.jsx
@@ -1,13 +1,33 @@
 import NavBar from "../../components/navBar/NavBar";
 import Header from "../../components/header/Header";
 import styles from "./Profil.module.css";
+import { useTheme } from "../../contexts/ThemeContext";
 
 function Profil() {
+  const { isLight } = useTheme();
   return (
-    <div>
+    <div className={isLight ? `${styles.lightMode}` : `${styles.darkMode}`}>
       <NavBar />
       <Header />
-      <h1 className={styles.titleH1}>Encore rien...</h1>
+      <div className={styles.outline}>
+        <div className={styles.container}>
+          <form className={styles.formProfil}>
+            <label>
+              <p>Email :</p>
+              <input type="text" name="name" />
+            </label>
+            <br />
+            <label>
+              <p>Password :</p>
+              <input type="text" name="password" />
+            </label>
+            <br />
+            <button className={styles.button} type="button">
+              Login
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
